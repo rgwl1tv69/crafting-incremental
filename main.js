a1503939cea4611accfede8da1898d01cbb99538
rgwl1tv69 <168993357+rgwl1tv69@users.noreply.github.com>
2024-09-22 19:47:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 var money = 1000;
 var pollution = 0;
-var machines = [];
+// format: [id,amount]
+var machines = [
+  ["drill",1]
+];
 // format: [ItemName,Description,Cost,Sell/Buy,id]
 var items = [
   ["Drill MK1","mines ores",250,"buy","drill"],
-  ["Raw Copper","smelt me",2,"sell","copper_ore"],
-  ["Raw Iron","smelt me",4,"sell","iron_ore"],
+  ["Raw Copper","smelt me",1,"sell","copper_ore"],
+  ["Raw Iron","smelt me",2,"sell","iron_ore"],
   ["Copper Ingot","very useful",5,"sell","copper_ingot"],
   ["Iron Ingot","very useful",10,"sell","iron_ingot"],
   ["Furnace","used to smelt ores",100,"buy","furnace"],
@@ -18,14 +21,29 @@ var inventory = [
   ["assembler",1],
   ["furnace",1],
 ];
-var function inventoryHasItem(id) {
-  for (let i = 0; i < inventory.length; i++) {
-    if (inventory[i][0] == id) {
+var function listHasItem(id,list) {
+  for (let i = 0; i < table.length; i++) {
+    if (list[i][0] == id) {
       return i;
     }
   };
   return false;
 };
-var function tick() {
-  
+var function addItemToInventory(id,amount) {
+  let index = listHasItem();
+  if (!index) {
+    inventory.push([id,amount]);
+  } else {
+    inventory[index][1] += amount
+  }
 };
+var function tick() {
+  let booleans = [
+    listHasItem("drill",machines)
+  ]
+  if (booleans[0]) {
+    addItemToInventory("iron_ore",machines[booleans[0]][1]);
+    addItemToInventory("copper_ore",machines[booleans[0]][1]);
+  };
+};
+setInterval(tick,1000)
